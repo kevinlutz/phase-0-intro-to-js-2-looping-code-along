01e742caacc509f6c3855db2b4adffa23553fb6d
+ <+>
2021-08-06 15:30:49 -0500

    Learned about for loops and while loops

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+
+function writeCards(arr, eventName) {
+  const messages = [];
+  for (let i = 0; i < arr.length; i++) {
+    messages.push(`Thank you, ${arr[i]}, for the wonderful ${eventName} gift!`);
+  }
+  return messages;
+}
+
+let num = 10;
+while (num >= 0) {
+  console.log(num);
+  num--;
+}
